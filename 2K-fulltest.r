936dfe0ff75f513e83334736cdd95ca3e2932fe7
leonStone <lmyqq2008@hotmail.com>
2022-08-10 17:48:00 +0800

    Update 2K-fulltest.r
    initial

--- 2K-fulltest.r
+++ 2K-fulltest.r
@@ -8,8 +8,10 @@ setwd("D:/rong-paper/2022COXrealdata")
 
 #require('mice')
 require('glmnet')
-#require('BB') # spg
+require('BB') # spg
 require('optimParallel')
+require('survivalROC')
+require('survC1')
 
 lambda_1_seq <-seq(2^-8,2^-3,length.out = 5)
 #lambda_1_seq <-seq(2^-7,2^-2,length.out = 10)
@@ -41,18 +43,17 @@ clin <- clin[id0,]
 rm(id0)
 
 ## 2K  
-subid <- 1:2000 
-dat <- dat[,subid]
+subid <- ncol(dat)#1:2000 
+dat <- dat[,1:subid]
 
 ## repetition
 nre <- 1000
-nam <- paste(format(Sys.time(), "%b%d"),nre,sep=';')
+nam <- paste(subid,format(Sys.time(), "%y%m%d"),sep='-')
 RES <- matrix(0,ncol=(3+5+20162))[-1,] 
 #RES_lasso <- matrix(0,ncol=(1+5+20162))[-1,]
 #RES_cosso <- matrix(0,ncol=(1))[-1,]
-LH<-matrix(0,ncol=3)[-1,]
-AUC<-matrix(0,ncol=3)[-1,]
-Cs<-matrix(0,ncol=3)[-1,]
+
+LH_PGKM <- AUC_PGKM <- ROC_PGKM <- Cs_PGKM <- c()
 FPS_PGKM<-c()
 TPS_PGKM<-c()
 #FPS_LASSO<-c()
@@ -61,7 +62,7 @@ TPS_PGKM<-c()
 #TPS_COSSO<-c()
 pgckmTime <- matrix(0,ncol=1)[-1,] ## PGKM takes time
 
-sink(file=nam)
+sink(file=paste(nre,nam,'.log',sep=''))
 for(re in 1:nre){
 	set.seed(re)
 ########### attention! original data need initial every repeat	  
@@ -70,7 +71,7 @@ for(re in 1:nre){
 	X<-apply(as.matrix(clin[,3:7]),2,as.numeric)  # 
 	Z<-apply(dat,2,as.numeric)
 
-#########训练集
+#########train set
 	train<- sample(1:nrow(Z),nrow(Z)*0.6)
 	remain_X <- as.matrix(X[-train,])
 	remain_Z <- as.matrix(Z[-train,])
@@ -82,7 +83,7 @@ for(re in 1:nre){
 	TIM <- TIM[train]
 	TAU <- TAU[train]
 
-#######验证集和测试集
+#######vali and test set
 	valid<- sample(1:nrow(remain_Z), nrow(remain_Z)*0.5)
 	Xvalid <- as.matrix(remain_X[valid,])
 	XT <- as.matrix(remain_X[-valid,])
@@ -103,23 +104,30 @@ for(re in 1:nre){
 	pgckmTime2 <- Sys.time()
 	pgckmtime <- round(difftime(pgckmTime2,pgckmTime1,units='secs'))
 	pgckmTime <- rbind(pgckmTime,as.vector(pgckmtime))
+	save(qt,file=paste(nam,'qt.RData',sep='-'))
+	
 	RT<-Rf(TIMT)$R;lenT<-Rf(TIMT)$len
 	hath <- hathaf(XT,Z,ZT,RT,lenT,as.vector(qt$ALPHA),as.vector(qt$BETA),as.vector(qt$DELTA))
 	#likelihood
 	hat_h <- hath$hat_h 
 	hat_A <- hath$hat_A 
 	LH1 <-(1/Ntest)* (t(TAUT) %*% ( XT%*%as.vector(qt$BETA)+hat_h-hat_A ))
+	LH_PGKM <- c(LH_PGKM,LH1)
+	
 	#AUC
 	cutoff<-max(TIMT)*0.9
 	mark<-XT%*%as.vector(qt$BETA)+hat_h
-	PGKM_ROC<-survivalROC(Stime=TIMT,status = TAUT,marker = mark,predict.time = cutoff,span=0.25*100^(-0.2))
-	PGKM_AUC<-PGKM_ROC$AUC
+	ROC_PGKM<-survivalROC(Stime=TIMT,status = TAUT,marker = mark,predict.time = cutoff,span=0.25*100^(-0.2))
+	AUC_PGKM <- c(AUC_PGKM,ROC_PGKM$AUC)
+	
 	FPS_PGKM<-rbind(FPS_PGKM,PGKM_ROC$FP)
 	TPS_PGKM<-rbind(TPS_PGKM,PGKM_ROC$TP)
+	
 	#C-statistics
 	ta<-quantile(TIMT,0.7)
 	mydata<-cbind(TIMT,TAUT,mark)
-	PGKM_C<-Est.Cval(mydata, ta, nofit=TRUE)$Dhat
+	Cs_PGKM<-c(Cs_PGKM,Est.Cval(mydata, ta, nofit=TRUE)$Dhat)
+	
 	## PGKM-cox result	  
 	RES <- rbind(RES, c(as.vector(qt$LAMBDA),as.vector(qt$BETA),as.vector(qt$DELTA)))
 
@@ -127,7 +135,8 @@ for(re in 1:nre){
 	print(paste('#### ',date(),'##re=',re,' has done ##PGKM takes',pgckmtime,'secs##'))
 	rm(.Random.seed)
 
-	save(FPS_PGKM,TPS_PGKM,FPS_LASSO,TPS_LASSO,FPS_COSSO,TPS_COSSO,LH,AUC,Cs,RES,RES_lasso,file=paste(nam,'.RData',sep=''))
+	save(FPS_PGKM,TPS_PGKM,LH_PGKM,AUC_PGKM,Cs_PGKM,RES,file=paste(nam,'res.RData',sep='-'))
 }
 sink()  
 
+print('finished'!)
